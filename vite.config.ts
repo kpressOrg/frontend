d8108aa692a37db07bffef13f7a1f931ef233b11
Я♡M...∞ <18311071+ru44@users.noreply.github.com>
2025-02-17 13:58:28 +0300

    feat: fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    tailwindcss(),
     AutoImport({
       imports: ['vue', 'vue-router', 'pinia'],
       dts: true,
